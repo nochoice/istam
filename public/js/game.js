ce4f42879467d30beeff4c9a862618327606d08e
Roman Kora <roman.kora@konicaminolta.cz>
2015-11-18 16:02:35 +0100

    User data

--- public/js/game.js
+++ public/js/game.js
@@ -4,9 +4,12 @@ var socket = io.connect('http://localhost:3700');
 var joinBtn = document.querySelector('.js-btn-join');
 joinBtn.addEventListener('click', join);
 
+socket.emit('player-data', enviroment);
+
 function join(e) {
-    socket.emit('join', enviroment);
+    socket.emit('join', enviroment, joinCb);
 }
 
-
-console.log(enviroment);
+function joinCb(data) {
+    console.log(data);
+}
